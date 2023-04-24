e9dad7729c8c0b0fdfb832e06281ccca76c87519
meshach5667 <129974200+meshach5667@users.noreply.github.com>
2023-04-24 14:45:55 +0100

    Add files via upload

---
+++ A code to tell you the exact year you will turn hundred years.py
@@ -0,0 +1,8 @@
+#A code to tell you your age, the amount of years left for you to turn hundred years and the exact year you are going to turn hundred years
+age=int(input("Enter birth year: "))
+age1=2023-(age)
+print("your are",age1 ,"years old")
+yr=(100-(age1))
+print("You have", yr, "left to turn hundred")
+hundred=(yr+2023)
+print("You will turn hundred in the year ",hundred)
